8481969aeaa23cfaf48ae8a06246d8ee8a5c6255
sinryuji <siler2474@gmail.com>
2023-06-26 17:39:47 +0900

    feat: nest-socketio room

--- nest-socketio/server/src/events/events.gateway.ts
+++ nest-socketio/server/src/events/events.gateway.ts
@@ -9,7 +9,16 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Namespace, Server, Socket } from 'socket.io';
+import { Namespace, Socket } from 'socket.io';
+
+
+interface MessagePayload {
+  roomName: string;
+  message: string;
+}
+
+
+let createdRooms: string[] = [];
 
 
 @WebSocketGateway({
@@ -24,38 +33,23 @@ export class EventsGateway
   private logger = new Logger('Gateway');
 
 
-  @WebSocketServer() nsp: Server;
+  @WebSocketServer() nsp: Namespace;
+
 
   afterInit() {
-    // this.nsp.adapter.on('create-room', (room) => {
-    //   this.logger.log(`"Room:${room}"이 생성되었습니다.`);
-    // });
-    this.nsp.on('create-room', (room) => {
-      this.logger.log(`"Room:${room}"이 생성되었습니다.`);
-    });
+    this.nsp.adapter.on('delete-room', (room) => {
+      this.logger.log('delete-room 이벤트 발생');
+      const deletedRoom = createdRooms.find(
+        (createdRoom) => createdRoom === room,
+      );
+      if (!deletedRoom) return;
 
 
-    // this.nsp.adapter.on('join-room', (room, id) => {
-    //   this.logger.log(`"Socket:${id}"이 "Room:${room}"에 참여하였습니다.`);
-    // });
-    this.nsp.on('join-room', (room, id) => {
-      this.logger.log(`"Socket:${id}"이 "Room:${room}"에 참여하였습니다.`);
-    });
-
-
-    // this.nsp.adapter.on('leave-room', (room, id) => {
-    //   this.logger.log(`"Socket:${id}"이 "Room:${room}"에서 나갔습니다.`);
-    // });
-    this.nsp.on('leave-room', (room, id) => {
-      this.logger.log(`"Socket:${id}"이 "Room:${room}"에서 나갔습니다.`);
-    });
-
-
-    // this.nsp.adapter.on('delete-room', (roomName) => {
-    //   this.logger.log(`"Room:${roomName}"이 삭제되었습니다.`);
-    // });
-    this.nsp.on('delete-room', (roomName) => {
-      this.logger.log(`"Room:${roomName}"이 삭제되었습니다.`);
+      this.logger.log('룸 삭제!');
+      this.nsp.emit('delete-room', deletedRoom);
+      createdRooms = createdRooms.filter(
+        (createdRoom) => createdRoom !== deletedRoom,
+      ); // 유저가 생성한 room 목록 중에 삭제되는 room 있으면 제거
     });
 
 
@@ -65,11 +59,6 @@ export class EventsGateway
 
   handleConnection(@ConnectedSocket() socket: Socket) {
     this.logger.log(`${socket.id} 소켓 연결`);
-
-
-    socket.broadcast.emit('message', {
-      message: `${socket.id}가 들어왔습니다.`,
-    });
   }
 
 
@@ -81,9 +70,69 @@ export class EventsGateway
   @SubscribeMessage('message')
   handleMessage(
     @ConnectedSocket() socket: Socket,
-    @MessageBody() message: string,
+    @MessageBody() { roomName, message }: MessagePayload,
   ) {
-    socket.broadcast.emit('message', { username: socket.id, message });
+    socket.broadcast
+      .to(roomName)
+      .emit('message', { username: socket.id, message });
+
+
     return { username: socket.id, message };
+  }
+
+
+  @SubscribeMessage('room-list')
+  handleRoomList() {
+    return createdRooms;
+  }
+
+
+  @SubscribeMessage('create-room')
+  handleCreateRoom(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() roomName: string,
+  ) {
+    const exists = createdRooms.find((createdRoom) => createdRoom === roomName);
+    if (exists) {
+      return { success: false, payload: `${roomName} 방이 이미 존재합니다.` };
+    }
+
+
+    socket.join(roomName); // 기존에 없던 room으로 join하면 room이 생성됨
+    createdRooms.push(roomName); // 유저가 생성한 room 목록에 추가
+    this.nsp.emit('create-room', roomName); // 대기실 방 생성
+
+
+    return { success: true, payload: roomName };
+  }
+
+
+  @SubscribeMessage('join-room')
+  handleJoinRoom(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() roomName: string,
+  ) {
+    socket.join(roomName); // join room
+    socket.broadcast
+      .to(roomName)
+      .emit('message', { message: `${socket.id}가 들어왔습니다.` });
+
+
+    return { success: true };
+  }
+
+
+  @SubscribeMessage('leave-room')
+  handleLeaveRoom(
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() roomName: string,
+  ) {
+    socket.leave(roomName); // leave room
+    socket.broadcast
+      .to(roomName)
+      .emit('message', { message: `${socket.id}가 나갔습니다.` });
+
+
+    return { success: true };
   }
 }
